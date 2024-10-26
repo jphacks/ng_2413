17f89f51adcdf80fc473636898b3f28e6469c027
sulfffffer <136565943+sulfffffer@users.noreply.github.com>
2024-10-26 17:26:02 +0900

    fix app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
-=======
-from flask import Flask, render_template, redirect, url_for, flash, request
->>>>>>> Stashed changes
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -17,10 +13,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # ログインしていない場合にリダイレクトするページ
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 # データベースモデルのインポート
 from models import User
 
